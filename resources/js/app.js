53ec71821c62268a7e48acce94fca3e5e79955ad
Loris Leiva <loris.leiva@gmail.com>
2019-09-10 16:01:16 +0100

    Episode 0103

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 require('./bootstrap');
 import Vue from 'vue'
+import App from '@components/App'
 
 const app = new Vue({
     el: '#app',
+    render: h => h(App),
 });
---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Paparazzi</title>
+        
+        <script src="{{ mix('js/app.js') }}" defer></script>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    </head>
+    <body>
+        <div id="app"></div>
+    </body>
+</html>
