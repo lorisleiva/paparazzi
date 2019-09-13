30edb3711569eaead5ed877d6bf37aee9b5de58f
Loris Leiva <loris.leiva@gmail.com>
2019-09-13 11:16:41 +0100

    Episode 0105

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,20 @@
 const mix = require('laravel-mix');
+const purgecss = require('@fullhuman/postcss-purgecss')({
 
+   // Specify the paths to all of the template files in your project 
+   content: [
+     './resources/**/*.blade.php',
+     './resources/**/*.vue',
+   ],
+ 
+   // Include any special characters you're using in this regular expression
+   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+ })
+ 
 mix.js('resources/js/app.js', 'public/js')
    .postCss('resources/css/app.css', 'public/css', [
       require('tailwindcss'),
+      ...mix.inProduction() ? [purgecss] : [],
       require('autoprefixer'),
    ])
 
